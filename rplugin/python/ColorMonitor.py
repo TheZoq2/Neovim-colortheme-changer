d43d37888c828af141278e1c45f840a46422a41e
TheZoq2 <frans.skarman@gmail.com>
2016-02-20 17:38:14 +0100

    Attempt to fix color stuff

--- rplugin/python/ColorMonitor.py
+++ rplugin/python/ColorMonitor.py
@@ -1,8 +1,11 @@
 import time
+
 import fcntl
+
 import os
 import signal
 import os
+import random
 
 import neovim
 
@@ -17,18 +20,20 @@ class ColorChanger(object):
         self.vim = vim
         
 
-    @neovim.command('ReadNewColorscheme', range='', nargs='*', sync=True)
+    @neovim.command('ReadNewColorscheme', range='', nargs='*', sync=False)
     def readColorScheme(self, args, range):
-        f = open(THEME_PATH, 'r')
-
-        colorscheme = f.readline().replace("\n", "")
-        self.vim.command("colorscheme {}".format(colorscheme))
-
-        f.close()
+        self.vim.command("so " + THEME_PATH)
 
 
+    @neovim.command('TestHandler', range='', nargs='*', sync=False)
     def folderChangeHandler(self, signum, frame):
-        self.vim.command("ReadNewColorscheme")
+        #self.vim.command("ReadNewColorscheme")
+        self.vim.command("so " + THEME_PATH)
+        #self.readColorScheme(None, None)
+        #self.vim.current.line = "{}".format(random.random())
+        f = open("/tmp/colors/debug", "w")
+        f.write("{}".format(random.random()))
+        f.close()
         
 
     def ensureFilesExist(self):
@@ -40,17 +45,17 @@ class ColorChanger(object):
             #Create the file with the current theme in it
             f = open(THEME_PATH, 'w')
 
-            f.write(self.vim.command_output("colorscheme").replace("\n", ""))
+            f.write("colorscheme " + self.vim.command_output("colorscheme").replace("\n", ""))
 
             f.close()
             
 
-
-
-
     @neovim.command('StartColorPoll', range='', nargs='*', sync=False)
     def command_handler(self, args, range):
         self.ensureFilesExist();
+
+        #read the current theme
+        self.folderChangeHandler(None, None)
 
         signal.signal(signal.SIGIO, self.folderChangeHandler)
         fd = os.open(FOLDER_NAME,  os.O_RDONLY)
@@ -61,4 +66,5 @@ class ColorChanger(object):
         #Sleep indefinetly while waiting for change events
         while True:
             time.sleep(10000)
+
 
